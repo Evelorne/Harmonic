ba0bc932a5f0c78d856f572eef3f29a62b073fc6
Evelorne <tayfundaldik@gmail.com>
2022-09-11 22:25:44 +0300

    harmonic numbers

--- src/harmonica.java
+++ src/harmonica.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class harmonica {
     public static void main(String[] args) {
        double n;
-       double c = 0;
        double w = 1;
         Scanner a =new Scanner(System.in);
         System.out.println("Enter a number: ");
@@ -12,7 +11,6 @@ public class harmonica {
         for (double i = 1; i <= n; i++) {
             i++;
             w = w+(1/i);
-
         }
         System.out.println(w);
     }
